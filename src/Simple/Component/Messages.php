5ffa8a670f0400c0050d20ea3314e4f7d1da4dae
accgit <zdenek.papucik@gmail.com>
2019-10-02 07:09:32 +0200

    Update

---
+++ src/Simple/Component/Messages.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * Drago Extension
+ * Package built on Nette Framework
+ */
+namespace Drago\Simple\Component;
+
+use Drago\Simple\Base\Sessions;
+use Nette\Http\SessionSection;
+
+
+/**
+ * Flash message.
+ */
+trait Messages
+{
+	use Sessions;
+
+	/** @var string */
+	private $fm = 'fm';
+
+
+	private function fmId(): SessionSection
+	{
+		return $this->session()
+			->getSection($this->fm);
+	}
+
+
+	/**
+	 * Save the message.
+	 */
+	public function flashMessage(string $message): SessionSection
+	{
+		$sessions = $this->fmId();
+		$sessions->message = $message;
+		$sessions->setExpiration('5 second');
+		return $sessions;
+	}
+
+
+	/**
+	 * Send message to template.
+	 */
+	public function getFlashMessage(): ?string
+	{
+		return $this->fmId()
+			->message;
+	}
+}
